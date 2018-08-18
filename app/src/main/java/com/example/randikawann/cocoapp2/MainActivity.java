6dfce5aa79d19d02615bde7ce0681cad7ba76e0c
randikawann <randikawann@gmail.com>
2018-08-17 22:47:26 -0500

    desgin meterials version compatible make

--- app/src/main/java/com/example/randikawann/cocoapp2/MainActivity.java
+++ app/src/main/java/com/example/randikawann/cocoapp2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.randikawann.cocoapp2;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Check if we're running on Android 5.0 or higher
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            //Call some material design  APIs here
+        }else{  //For Below API 21
+            //Implement this features without material design
+        }
     }
 }
