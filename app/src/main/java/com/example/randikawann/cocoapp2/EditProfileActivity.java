9c885cc88b6aa74329fbc5869bba781ffd3b10f1
randikawann <randikawann@gmail.com>
2018-08-27 00:50:59 -0500

    add change to branch for test merge

--- app/src/main/java/com/example/randikawann/cocoapp2/EditProfileActivity.java
+++ app/src/main/java/com/example/randikawann/cocoapp2/EditProfileActivity.java
@@ -60,6 +60,8 @@ public class EditProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //add change to branch 02soveimgretrieve
+        //merge it to 02editprofile
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
